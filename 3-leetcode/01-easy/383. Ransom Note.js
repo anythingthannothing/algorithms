b2b9bf335ee343c6d8c0d7f2159ab0164f6961c9
KimKeonU <anythingthannothing@gmail.com>
2022-07-26 15:08:42 +0900

    Solve 3 problems and edit README.md

---
+++ 3-leetcode/01-easy/383. Ransom Note.js
@@ -0,0 +1,27 @@
+// Using Hash Map => O(n)
+const canConstruct = (ransomNote, magazine) => {
+    let magazineMap = new Map();
+    
+    for (let i = 0, len = magazine.length; i < len; i++) {
+        magazineMap.set(magazine[i], magazineMap.get(magazine[i]) + 1 || 1);
+    }
+    
+    for (let i = 0, len = ransomNote.length; i < len; i++) {
+        if (magazineMap.get(ransomNote[i]) < 0 || !magazineMap.get(ransomNote[i])) return false;
+        magazineMap.set(ransomNote[i], magazineMap.get(ransomNote[i]) - 1);
+    }
+    return true;
+};
+
+// Using Brute Force? => O(n^2)
+// const canConstruct = (ransomNote, magazine) => {
+//     let temp;
+//     for (let i = 0, len = magazine.length; i < len; i++) {
+//         temp = magazine[i];
+//         if (ransomNote.indexOf(temp) >= 0) ransomNote = ransomNote.replace(temp, "");
+//         if (!ransomNote.length) return true;
+//     }
+//     return false;
+// };
+
+
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 알고리즘 공부 기록과 풀이
 
-1. 프로그래머스 22/07/25 7,232th | 1,367 Point | 91 Solved
-2. LeetCode 22/07/25 Easy 2 / 582 | Medium 2 / 1251 | Hard 1 / 513 | 5 Solved
+1. 프로그래머스 22/07/26 7,232th | 1,367 Point | 91 Solved
+2. LeetCode 22/07/26 Easy 4 / 582 | Medium 3 / 1251 | Hard 1 / 513 | 8 Solved
 
 - 22/06/26 프로그래머스 Lv 1 All Solved
