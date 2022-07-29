f9555375b799a38910ff876cfdee7f7d1464a75f
KimKeonU <anythingthannothing@gmail.com>
2022-07-29 14:45:48 +0900

    Solve 3 problems and edit READMD.md

---
+++ 3-leetcode/01-easy/121. Best Time to Buy and Sell Stock.js
@@ -0,0 +1,12 @@
+// T.C: O(n) | S.C: O(1)
+const maxProfit = (prices) => {
+    let min = Infinity,
+        profit = 0;
+    for (let i = 0, len = prices.length; i < len; i++) {
+      if (prices[i] < min) {
+        min = prices[i];
+      }
+      profit = Math.max(profit, prices[i] - min);
+    }
+    return profit;
+  };
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 알고리즘 공부 기록과 풀이
 
-22/07/28
+22/07/29
 
 1. 프로그래머스 7,232th | 1,367 Point | 91 Solved
-2. LeetCode 1,682,257th | 15 Solved(Easy 9 / Medium 5 / Hard 1)
+2. LeetCode 1,478,279th | 18 Solved(Easy 11 / Medium 6 / Hard 1)
