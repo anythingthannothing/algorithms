bbe55fa90ce923f91c91358ab37e41927cb0f28f
KimKeonU <anythingthannothing@gmail.com>
2022-08-01 14:57:43 +0900

    8/1 Solve 3 problems and edit README.md

---
+++ 3-leetcode/02-medium/62. Unique Paths.js
@@ -0,0 +1,29 @@
+// Solution Using Dynamic Programming
+const uniquePaths = (m, n) => {
+    if (m === 1 || n === 1) return 1;
+    let dp = Array.from({length: m}, v => Array.from({length: n}, v => 0));
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        if (i === 0) dp[i][j] = 1;
+        else if (j === 0) dp[i][j] = 1;
+        else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    return dp[m - 1][n - 1];
+  };
+  
+  
+  // Solution Using Combination
+  const uniquePaths2 = (m, n) => {
+    let answer = 1;
+    if (m === 1 || n === 1) return answer;
+    m--;
+    n--;
+    let sum = m + n;
+    while (sum > 1) {
+      answer *= sum--;
+      if (m > 1) answer /= m--;
+      if (n > 1) answer /= n--;
+    }
+    return answer;
+  };
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 알고리즘 공부 기록과 풀이
 
-22/07/31
+22/08/01
 
 1. 프로그래머스 7,232th | 1,367 Point | 91 Solved
-2. LeetCode 1,240,277th | 28 Solved(Easy 17 / Medium 10 / Hard 1)
+2. LeetCode 1,102,155th | 31 Solved(Easy 18 / Medium 12 / Hard 1)
