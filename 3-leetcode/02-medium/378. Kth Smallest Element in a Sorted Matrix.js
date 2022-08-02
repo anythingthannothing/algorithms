ff96eca85b74787c4f098fe702b31b33840325e4
KimKeonU <anythingthannothing@gmail.com>
2022-08-02 15:19:25 +0900

    Solve a problem and edit README.md

---
+++ 3-leetcode/02-medium/378. Kth Smallest Element in a Sorted Matrix.js
@@ -0,0 +1,11 @@
+// Solution Using Sorting => T.C: O(nlogn) | S.C: O(n^2)
+let kthSmallest = (matrix, k) => {
+    let newMatrix = [];
+    let len = matrix.length;
+    for (let i = 0; i < len; i++) {
+      for (let j = 0; j < len; j++) {
+        newMatrix.push(matrix[i][j]);
+      }
+    }
+    return newMatrix.sort((a, b) => a - b)[k - 1];
+  };
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 알고리즘 공부 기록과 풀이
 
-22/08/01
+22/08/02
 
 1. 프로그래머스 7,232th | 1,367 Point | 91 Solved
-2. LeetCode 1,102,155th | 31 Solved(Easy 18 / Medium 12 / Hard 1)
+2. LeetCode 1,046,663th | 32 Solved(Easy 18 / Medium 13 / Hard 1)
