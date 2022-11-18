bb6f5655f690d9c8edc9d53067aebe34eaeb5b86
KimKeonU <anythingthannothing@gmail.com>
2022-11-18 11:52:33 +0900

    solve: leetcode daily challenge and programmers lv-1

--- 3-leetcode/02-medium/223-rectangle-area.js
+++ 3-leetcode/02-medium/223-rectangle-area.js
@@ -1,6 +1,6 @@
 const computeArea = function (ax1, ay1, ax2, ay2, bx1, by1, bx2, by2) {
-  const rectA = Math.abs(ax2 - ax1) * Math.abs(ay2 - ay1);
-  const rectB = Math.abs(bx2 - bx1) * Math.abs(by2 - by1);
+  const rectA = (ax2 - ax1) * (ay2 - ay1);
+  const rectB = (bx2 - bx1) * (by2 - by1);
 
   const rectCsX = Math.min(ax2, bx2) - Math.max(ax1, bx1);
   const rectCsY = Math.min(ay2, by2) - Math.max(ay1, by1);
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 알고리즘 공부 기록과 풀이
 
-22/11/17
+22/11/18
 
 1. 프로그래머스 6,981th | 95 Solved | 1,377 Point
 
-2. LeetCode 308,753th | 179 Solved(Easy 61 / Medium 94 / Hard 24)
+2. LeetCode 307,015th | 180 Solved(Easy 62 / Medium 94 / Hard 24)
 - https://leetcode.com/anythingthannothing/
