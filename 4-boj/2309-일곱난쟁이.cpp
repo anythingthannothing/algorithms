59637e3e4587a6314b0d1e92ba0acf9df00ec585
KeonU Kim <anythingthannothing@gmail.com>
2022-11-23 13:41:23 +0900

    solve: leetcode daily challenge

--- 4-boj/2309-일곱난쟁이.cpp
+++ 4-boj/2309-일곱난쟁이.cpp
@@ -1,3 +1,4 @@
+// Combination을 활용한 풀이
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -44,3 +45,32 @@ int main()
         cout << i << ' ';
     return 0;
 }
+
+// Permutation을 활용한 풀이
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// int a[9];
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+//     for (int i = 0; i < 9; i++)
+//     {
+//         cin >> a[i];
+//     }
+//     sort(a, a + 9);
+//     do
+//     {
+//         int sum = 0;
+//         for (int i = 0; i < 7; i++)
+//             sum += a[i];
+//         if (sum == 100)
+//             break;
+//     } while (next_permutation(a, a + 9));
+//     for (int i = 0; i < 7; i++)
+//         cout << a[i] << "\n";
+//     return 0;
+// }
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 알고리즘 공부 기록과 풀이
 
-22/11/22
+22/11/23
 
 1. 프로그래머스 6,981th | 95 Solved | 1,377 Point
 
-2. LeetCode 299,980th | 184 Solved(Easy 62 / Medium 96 / Hard 26)
+2. LeetCode 298,217th | 185 Solved(Easy 62 / Medium 97 / Hard 26)
 - https://leetcode.com/anythingthannothing/
