988bfea5d6a724150dd624c15fad7c2a765cbad1
KeonU Kim <anythingthannothing@gmail.com>
2022-12-15 01:31:18 +0900

    solve: leetcode daily challenge

---
+++ 3-leetcode/02-medium/198-house-robber.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int rob(vector<int> &A)
+  {
+    int prev2 = 0, prev = 0, cur = 0;
+    for (auto i : A)
+    {
+      cur = max(prev, i + prev2);
+      prev2 = prev;
+      prev = cur;
+    }
+    return cur;
+  }
+};
