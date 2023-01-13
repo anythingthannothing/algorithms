74eefbe072647b10fc36f1950e9621176668f264
KeonU Kim <anythingthannothing@gmail.com>
2023-01-13 14:49:50 +0900

    solve: leetcode daily challenge & boj

---
+++ 4-boj/4-platinum/3197-백조의-호수.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair<int, int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+int visited_swan[1500][1500], visited[1500][1500], r, c, cnt, swanY, swanX, y, x;
+char lake[1500][1500];
+queue<pair<int, int>> waterQ, water_tempQ, swanQ, swan_tempQ;
+
+void Qclear(queue<pair<int, int>> &q)
+{
+  queue<pair<int, int>> empty;
+  swap(q, empty);
+}
+void water_melting()
+{
+  while (waterQ.size())
+  {
+    tie(y, x) = waterQ.front();
+    waterQ.pop();
+    for (auto dir : dirs)
+    {
+      int ny = y + dir.first;
+      int nx = x + dir.second;
+      if (ny < 0 || ny >= r || nx < 0 || nx >= c || visited[ny][nx])
+        continue;
+      if (lake[ny][nx] == 'X')
+      {
+        visited[ny][nx] = 1;
+        water_tempQ.push({ny, nx});
+        lake[ny][nx] = '.';
+      }
+    }
+  }
+}
+
+bool move_swan()
+{
+  while (swanQ.size())
+  {
+    tie(y, x) = swanQ.front();
+    swanQ.pop();
+    for (auto dir : dirs)
+    {
+      int ny = y + dir.first;
+      int nx = x + dir.second;
+      if (ny < 0 || ny >= r || nx < 0 || nx >= c || visited_swan[ny][nx])
+        continue;
+      visited_swan[ny][nx] = 1;
+      if (lake[ny][nx] == '.')
+        swanQ.push({ny, nx});
+      else if (lake[ny][nx] == 'X')
+        swan_tempQ.push({ny, nx});
+      else if (lake[ny][nx] == 'L')
+        return true;
+    }
+  }
+  return false;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cin >> r >> c;
+
+  for (int i = 0; i < r; i++)
+  {
+    for (int j = 0; j < c; j++)
+    {
+      cin >> lake[i][j];
+      if (lake[i][j] == 'L')
+      {
+        swanY = i;
+        swanX = j;
+      }
+      if (lake[i][j] == '.' || lake[i][j] == 'L')
+        visited[i][j] = 1, waterQ.push({i, j});
+    }
+  }
+  swanQ.push({swanY, swanX});
+  visited_swan[swanY][swanX] = 1;
+  while (!move_swan())
+  {
+    water_melting();
+    waterQ = water_tempQ;
+    swanQ = swan_tempQ;
+    Qclear(water_tempQ);
+    Qclear(swan_tempQ);
+    cnt++;
+  }
+  cout << cnt << "\n";
+  return 0;
+}
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 알고리즘 공부 기록과 풀이
 
-23/01/12
+23/01/13
 
-1. LeetCode 230,141th | 233 Solved(Easy 74 / Medium 127 / Hard 32)
+1. LeetCode 229,040th | 234 Solved(Easy 74 / Medium 127 / Hard 33)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
 
-3. BOJ Silver III | 51 Solved
+3. BOJ Silver III | 52 Solved
