47a4ee9428d1161c2297e0e174c0865f0430c410
KeonU Kim <anythingthannothing@gmail.com>
2023-03-04 17:39:40 +0900

    ✏ solve: boj
    14888. 연산자 끼워넣기

---
+++ 4-boj/2-silver/14888-연산자-끼워넣기.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, a[11], pl, mi, mul, divi;
+int ret = -1000000001;
+int ret2 = 1000000001;
+
+void go(int index, int curr, int plus, int minus, int mul, int div)
+{
+  if (index == n - 1)
+  {
+    ret = max(ret, curr);
+    ret2 = min(ret2, curr);
+    return;
+  }
+  if (plus)
+    go(index + 1, curr + a[index + 1], plus - 1, minus, mul, div);
+  if (minus)
+    go(index + 1, curr - a[index + 1], plus, minus - 1, mul, div);
+  if (mul)
+    go(index + 1, curr * a[index + 1], plus, minus, mul - 1, div);
+  if (div)
+    go(index + 1, curr / a[index + 1], plus, minus, mul, div - 1);
+  return;
+}
+
+int main()
+{
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+
+  cin >> pl >> mi >> mul >> divi;
+
+  go(0, a[0], pl, mi, mul, divi);
+
+  cout << ret << '\n'
+       << ret2;
+
+  return 0;
+}
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 알고리즘 공부 기록과 풀이
 
-23/03/03
+23/03/04
 
 1. LeetCode 198,195th | 266 Solved(Easy 93 / Medium 139 / Hard 34)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
 
-3. BOJ Silver I | 78 Solved
+3. BOJ Silver I | 79 Solved
