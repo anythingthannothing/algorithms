b2da1271bde41ee040824f9b6cd3533e03a880c3
KeonU Kim <anythingthannothing@gmail.com>
2023-03-08 11:09:26 +0900

    ✏ solve: leetcode
    1912. 연속합

---
+++ 4-boj/2-silver/1912-연속합.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, sum, curr, ans = -1001;
+
+int main()
+{
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> curr;
+    sum += curr;
+    ans = max(ans, sum);
+    if (sum < 0)
+      sum = 0;
+  }
+
+  cout << ans;
+  return 0;
+}
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 알고리즘 공부 기록과 풀이
 
-23/03/07
+23/03/08
 
 1. LeetCode 197,011th | 268 Solved(Easy 93 / Medium 141 / Hard 34)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
 
-3. BOJ Silver I | 81 Solved
+3. BOJ Silver I | 82 Solved
