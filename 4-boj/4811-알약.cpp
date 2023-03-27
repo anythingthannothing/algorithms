25607250aa1907449e25c9e2617b8c048fc3f1b4
KeonU Kim <anythingthannothing@gmail.com>
2023-03-27 14:16:36 +0900

    ✏ solve: boj
    4811. 알약

---
+++ 4-boj/4811-알약.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+ll n, dp[31][31];
+
+ll go(int whole, int half)
+{
+  if (whole == 0 && half == 0)
+    return 1;
+  if (dp[whole][half])
+    return dp[whole][half];
+  ll &ret = dp[whole][half];
+  if (whole > 0)
+    ret += go(whole - 1, half + 1);
+  if (half > 0)
+    ret += go(whole, half - 1);
+  cout << whole << ": " << half << ": " << ret << "\n";
+  return ret;
+}
+
+int main()
+{
+  while (true)
+  {
+    cin >> n;
+    if (n == 0)
+      break;
+    cout << go(n, 0) << "\n";
+  }
+  return 0;
+}
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 알고리즘 공부 기록과 풀이
 
-23/03/26
+23/03/27
 
 1. LeetCode 196,192th | 269 Solved(Easy 94 / Medium 141 / Hard 34)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
 
-3. BOJ Gold V | 95 Solved
+3. BOJ Gold V | 96 Solved
