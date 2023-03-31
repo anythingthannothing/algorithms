bff5baae3e372c72623b61c115083c8abc599d8b
KeonU Kim <anythingthannothing@gmail.com>
2023-03-31 23:22:32 +0900

    ✏ solve: boj
    4781. 사탕 가게

--- 1-problem-solving-patterns/06-search-algorithms/binarySearch.js
+++ 1-problem-solving-patterns/06-search-algorithms/binarySearch.js
@@ -1,9 +1,9 @@
-  function binarySearch(n, arr) {
-    for (let start = 0, end = arr.length - 1; start <= end; ) {
-      let mid = Math.floor((start + end) / 2);
-      if (arr[mid] === n) return mid;
-      else if (arr[mid] > n) end = mid - 1;
-      else start = mid + 1;
-    }
-    return -1;
+function binarySearch(n, arr) {
+  for (let start = 0, end = arr.length - 1; start <= end; ) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === n) return mid;
+    else if (arr[mid] > n) end = mid - 1;
+    else start = mid + 1;
   }
+  return -1;
+}
