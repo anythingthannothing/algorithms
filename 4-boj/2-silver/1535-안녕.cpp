50505fab1c31aa74e5c1eb2c3ad8514fdcf276c2
KeonU Kim <anythingthannothing@gmail.com>
2023-04-15 17:21:48 +0900

    ✏ solve: boj
    1535. 안녕

---
+++ 4-boj/2-silver/1535-안녕.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, dp[101], cost[20], joy[20];
+
+int main()
+{
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+    cin >> cost[i];
+
+  for (int i = 0; i < n; i++)
+    cin >> joy[i];
+
+  for (int i = 0; i < n; i++)
+    for (int j = 100; j > cost[i]; j--)
+    {
+      dp[j] = max(dp[j], dp[j - cost[i]] + joy[i]);
+    }
+
+  cout << dp[100];
+
+  return 0;
+}
