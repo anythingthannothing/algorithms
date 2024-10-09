5b57bdd288b93c37b03e7ef96ee7dc454d8f346b
KimKeonU <anythingthannothing@gmail.com>
2024-10-09 13:20:51 +0900

    solve: leetcode
    daily challenge

---
+++ 3-leetcode/02-medium/cpp/921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int minAddToMakeValid(string s)
+    {
+        int openBrackets = 0;
+
+        int minAddsRequired = 0;
+        for (char c : s)
+        {
+            if (c == '(')
+            {
+                openBrackets++;
+            }
+            else
+            {
+                openBrackets > 0 ? openBrackets-- : minAddsRequired++;
+            }
+        }
+
+        return minAddsRequired + openBrackets;
+    }
+};
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 알고리즘 공부 기록과 풀이
 
-24/10/08
+24/10/09
 
-1. LeetCode 202,308th | 376 solved(Easy 151 / Medium 181 / Hard 44)
+1. LeetCode 200,774th | 377 solved(Easy 151 / Medium 182 / Hard 44)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
