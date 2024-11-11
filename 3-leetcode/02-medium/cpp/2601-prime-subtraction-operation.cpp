14dd7f4789fccd1532eaa994858f5f6383e014f1
KeonU Kim <anythingthannothing@gmail.com>
2024-11-11 10:11:25 +0900

    solve: leetcode
    daily challenge - prime number

---
+++ 3-leetcode/02-medium/cpp/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool primeSubOperation(vector<int> &nums)
+    {
+        int maxElement = *max_element(nums.begin(), nums.end());
+
+        vector<int> sieve(maxElement + 1, 1);
+        sieve[1] = 0;
+        for (int i = 2; i <= sqrt(maxElement + 1); i++)
+        {
+            if (sieve[i] == 1)
+            {
+                for (int j = i * i; j <= maxElement; j += i)
+                {
+                    sieve[j] = 0;
+                }
+            }
+        }
+
+        int currValue = 1;
+        int i = 0;
+        while (i < nums.size())
+        {
+            int difference = nums[i] - currValue;
+
+            if (difference < 0)
+            {
+                return false;
+            }
+
+            if (sieve[difference] == 1 or difference == 0)
+            {
+                i++;
+                currValue++;
+            }
+            else
+            {
+                currValue++;
+            }
+        }
+        return true;
+    }
+};
