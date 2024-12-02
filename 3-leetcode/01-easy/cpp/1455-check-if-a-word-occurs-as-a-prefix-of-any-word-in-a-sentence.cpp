502397ddb114e3ffdcf27d5eb07ff41632f7be16
KeonU Kim <anythingthannothing@gmail.com>
2024-12-02 10:24:21 +0900

    solve: leetcode
    daily challenge - string

---
+++ 3-leetcode/01-easy/cpp/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int isPrefixOfWord(string sentence, string searchWord)
+    {
+        istringstream sentenceStream(sentence);
+        string currentWord;
+
+        int wordPosition = 1;
+
+        while (sentenceStream >> currentWord)
+        {
+            if (currentWord.length() >= searchWord.length() &&
+                currentWord.compare(0, searchWord.length(), searchWord) == 0)
+            {
+                return wordPosition;
+            }
+            wordPosition++;
+        }
+
+        return -1;
+    }
+};
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 알고리즘 공부 기록과 풀이
 
-24/12/01
+24/12/02
 
-1. LeetCode 135,533th | 475 solved(Easy 174 / Medium 244 / Hard 57)
+1. LeetCode 135,082th | 476 solved(Easy 175 / Medium 244 / Hard 57)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
