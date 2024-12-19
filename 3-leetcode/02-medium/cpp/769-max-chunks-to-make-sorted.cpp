779f4ba1135dde7d175af186f8dd35c038dcd63c
KeonU Kim <anythingthannothing@gmail.com>
2024-12-19 11:23:29 +0900

    solve: leetcode
    daily challenge - max element

---
+++ 3-leetcode/02-medium/cpp/769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxChunksToSorted(vector<int> &arr)
+    {
+        int n = arr.size();
+        int chunks = 0, maxElement = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            maxElement = max(maxElement, arr[i]);
+
+            if (maxElement == i)
+            {
+                chunks++;
+            }
+        }
+
+        return chunks;
+    }
+};
--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 알고리즘 공부 기록과 풀이
 
-24/12/18
+24/12/19
 
-1. LeetCode 127,806th | 492 solved(Easy 178 / Medium 257 / Hard 57)
+1. LeetCode 127,414th | 493 solved(Easy 178 / Medium 258 / Hard 57)
 - https://leetcode.com/anythingthannothing/
 
 2. 프로그래머스 6,981th | 95 Solved | 1,377 Point
